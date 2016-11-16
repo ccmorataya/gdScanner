c8826e1376dc9fd4be8a6904e2c0bd32e730ef7b
Cristian Morataya <cris.morataya@gmail.com>
2016-11-16 09:51:53 -0600

    Created the function to extract the alphabet

--- scripts/index.gd
+++ scripts/index.gd
@@ -14,147 +14,147 @@ func _ready():
 # Handling the keyboard presed keys
 func _input(event):
 	if (!isProcessed):
-		if (event.is_action_pressed("charA") ):
+		if (event.is_action_pressed("charA") && !event.is_echo()):
 			text += "a"
-		elif (event.is_action_pressed("charB") ):
+		elif (event.is_action_pressed("charB") && !event.is_echo()):
 			text += "b"
-		elif (event.is_action_pressed("charC") ):
+		elif (event.is_action_pressed("charC") && !event.is_echo()):
 			text += "c"
-		elif (event.is_action_pressed("charD") ):
+		elif (event.is_action_pressed("charD") && !event.is_echo()):
 			text += "d"
-		elif (event.is_action_pressed("charE") ):
+		elif (event.is_action_pressed("charE") && !event.is_echo()):
 			text += "e"
-		elif (event.is_action_pressed("charF") ):
+		elif (event.is_action_pressed("charF") && !event.is_echo()):
 			text += "f"
-		elif (event.is_action_pressed("charG") ):
+		elif (event.is_action_pressed("charG") && !event.is_echo()):
 			text += "g"
-		elif (event.is_action_pressed("charH") ):
+		elif (event.is_action_pressed("charH") && !event.is_echo()):
 			text += "h"
-		elif (event.is_action_pressed("charI") ):
+		elif (event.is_action_pressed("charI") && !event.is_echo()):
 			text += "i"
-		elif (event.is_action_pressed("charJ") ):
+		elif (event.is_action_pressed("charJ") && !event.is_echo()):
 			text += "j"
-		elif (event.is_action_pressed("charK") ):
+		elif (event.is_action_pressed("charK") && !event.is_echo()):
 			text += "k"
-		elif (event.is_action_pressed("charL") ):
+		elif (event.is_action_pressed("charL") && !event.is_echo()):
 			text += "l"
-		elif (event.is_action_pressed("charM") ):
+		elif (event.is_action_pressed("charM") && !event.is_echo()):
 			text += "m"
-		elif (event.is_action_pressed("charN") ):
+		elif (event.is_action_pressed("charN") && !event.is_echo()):
 			text += "n"
-		elif (event.is_action_pressed("charO") ):
+		elif (event.is_action_pressed("charO") && !event.is_echo()):
 			text += "o"
-		elif (event.is_action_pressed("charP") ):
+		elif (event.is_action_pressed("charP") && !event.is_echo()):
 			text += "p"
-		elif (event.is_action_pressed("charQ") ):
+		elif (event.is_action_pressed("charQ") && !event.is_echo()):
 			text += "q"
-		elif (event.is_action_pressed("charR") ):
+		elif (event.is_action_pressed("charR") && !event.is_echo()):
 			text += "r"
-		elif (event.is_action_pressed("charS") ):
+		elif (event.is_action_pressed("charS") && !event.is_echo()):
 			text += "s"
-		elif (event.is_action_pressed("charT") ):
+		elif (event.is_action_pressed("charT") && !event.is_echo()):
 			text += "t"
-		elif (event.is_action_pressed("charU") ):
+		elif (event.is_action_pressed("charU") && !event.is_echo()):
 			text += "u"
-		elif (event.is_action_pressed("charV") ):
+		elif (event.is_action_pressed("charV") && !event.is_echo()):
 			text += "v"
-		elif (event.is_action_pressed("charW") ):
+		elif (event.is_action_pressed("charW") && !event.is_echo()):
 			text += "w"
-		elif (event.is_action_pressed("charX") ):
+		elif (event.is_action_pressed("charX") && !event.is_echo()):
 			text += "x"
-		elif (event.is_action_pressed("charY") ):
+		elif (event.is_action_pressed("charY") && !event.is_echo()):
 			text += "y"
-		elif (event.is_action_pressed("charZ") ):
+		elif (event.is_action_pressed("charZ") && !event.is_echo()):
 			text += "z"
-		elif (event.is_action_pressed("char1") ):
+		elif (event.is_action_pressed("char1") && !event.is_echo()):
 			text += "1"
-		elif (event.is_action_pressed("char2") ):
+		elif (event.is_action_pressed("char2") && !event.is_echo()):
 			text += "2"
-		elif (event.is_action_pressed("char3") ):
+		elif (event.is_action_pressed("char3") && !event.is_echo()):
 			text += "3"
-		elif (event.is_action_pressed("char4") ):
+		elif (event.is_action_pressed("char4") && !event.is_echo()):
 			text += "4"
-		elif (event.is_action_pressed("char5") ):
+		elif (event.is_action_pressed("char5") && !event.is_echo()):
 			text += "5"
-		elif (event.is_action_pressed("char6") ):
+		elif (event.is_action_pressed("char6") && !event.is_echo()):
 			text += "6"
-		elif (event.is_action_pressed("char7") ):
+		elif (event.is_action_pressed("char7") && !event.is_echo()):
 			text += "7"
-		elif (event.is_action_pressed("char8") ):
+		elif (event.is_action_pressed("char8") && !event.is_echo()):
 			text += "8"
-		elif (event.is_action_pressed("char9") ):
+		elif (event.is_action_pressed("char9") && !event.is_echo()):
 			text += "9"
-		elif (event.is_action_pressed("char0") ):
+		elif (event.is_action_pressed("char0") && !event.is_echo()):
 			text += "0"
-		elif (event.is_action_pressed("char!") ):
+		elif (event.is_action_pressed("char!") && !event.is_echo()):
 			text += "!"
-		elif (event.is_action_pressed("char@") ):
+		elif (event.is_action_pressed("char@") && !event.is_echo()):
 			text += "@"
-		elif (event.is_action_pressed("char#") ):
+		elif (event.is_action_pressed("char#") && !event.is_echo()):
 			text += "#"
-		elif (event.is_action_pressed("char$") ):
+		elif (event.is_action_pressed("char$") && !event.is_echo()):
 			text += "$"
-		elif (event.is_action_pressed("char%") ):
+		elif (event.is_action_pressed("char%") && !event.is_echo()):
 			text += "%"
-		elif (event.is_action_pressed("char^") ):
+		elif (event.is_action_pressed("char^") && !event.is_echo()):
 			text += "^"
-		elif (event.is_action_pressed("char&") ):
+		elif (event.is_action_pressed("char&") && !event.is_echo()):
 			text += "&"
-		elif (event.is_action_pressed("char*") ):
+		elif (event.is_action_pressed("char*") && !event.is_echo()):
 			text += "*"
-		elif (event.is_action_pressed("char(") ):
+		elif (event.is_action_pressed("char(") && !event.is_echo()):
 			text += "("
-		elif (event.is_action_pressed("char)") ):
+		elif (event.is_action_pressed("char)") && !event.is_echo()):
 			text += ")"
-		elif (event.is_action_pressed("char`") ):
+		elif (event.is_action_pressed("char`") && !event.is_echo()):
 			text += "`"
-		elif (event.is_action_pressed("char~") ):
+		elif (event.is_action_pressed("char~") && !event.is_echo()):
 			text += "~"
-		elif (event.is_action_pressed("char-") ):
+		elif (event.is_action_pressed("char-") && !event.is_echo()):
 			text += "-"
-		elif (event.is_action_pressed("char_") ):
+		elif (event.is_action_pressed("char_") && !event.is_echo()):
 			text += "_"
-		elif (event.is_action_pressed("charEq") ):
+		elif (event.is_action_pressed("charEq") && !event.is_echo()):
 			text += "="
-		elif (event.is_action_pressed("char+") ):
+		elif (event.is_action_pressed("char+") && !event.is_echo()):
 			text += "+"
-		elif (event.is_action_pressed("char[") ):
+		elif (event.is_action_pressed("char[") && !event.is_echo()):
 			text += "["
-		elif (event.is_action_pressed("char]") ):
+		elif (event.is_action_pressed("char]") && !event.is_echo()):
 			text += "]"
-		elif (event.is_action_pressed("char\\") ):
+		elif (event.is_action_pressed("char\\") && !event.is_echo()):
 			text += "\\"
-		elif (event.is_action_pressed("char{") ):
+		elif (event.is_action_pressed("char{") && !event.is_echo()):
 			text += "{"
-		elif (event.is_action_pressed("char}") ):
+		elif (event.is_action_pressed("char}") && !event.is_echo()):
 			text += "}"
-		elif (event.is_action_pressed("char|") ):
+		elif (event.is_action_pressed("char|") && !event.is_echo()):
 			text += "|"
-		elif (event.is_action_pressed("charSemiC") ):
+		elif (event.is_action_pressed("charSemiC") && !event.is_echo()):
 			text += ";"
-		elif (event.is_action_pressed("char'") ):
+		elif (event.is_action_pressed("char'") && !event.is_echo()):
 			text += "'"
-		elif (event.is_action_pressed("charColon") ):
+		elif (event.is_action_pressed("charColon") && !event.is_echo()):
 			text += ":"
-		elif (event.is_action_pressed("charDQ") ):
+		elif (event.is_action_pressed("charDQ") && !event.is_echo()):
 			text += "\""
-		elif (event.is_action_pressed("char,") ):
+		elif (event.is_action_pressed("char,") && !event.is_echo()):
 			text += ","
-		elif (event.is_action_pressed("char.") ):
+		elif (event.is_action_pressed("char.") && !event.is_echo()):
 			text += "."
-		elif (event.is_action_pressed("charSlash") ):
+		elif (event.is_action_pressed("charSlash") && !event.is_echo()):
 			text += "/"
-		elif (event.is_action_pressed("char<") ):
+		elif (event.is_action_pressed("char<") && !event.is_echo()):
 			text += "<"
-		elif (event.is_action_pressed("char>") ):
+		elif (event.is_action_pressed("char>") && !event.is_echo()):
 			text += ">"
-		elif (event.is_action_pressed("char?") ):
+		elif (event.is_action_pressed("char?") && !event.is_echo()):
 			text += "?"
-		elif (event.is_action_pressed("charSpace") ):
+		elif (event.is_action_pressed("charSpace") && !event.is_echo()):
 			text += " "
-		elif (event.is_action_pressed("charDel") ):
+		elif (event.is_action_pressed("charDel") && !event.is_echo()):
 			text.erase( (text.length()-1) ,1)
-		elif (event.is_action_pressed("enter") ):
+		elif (event.is_action_pressed("enter") && !event.is_echo()):
 			output += text + "\n"
 			labelOutput.set_text(output)
 			text = ""
@@ -162,13 +162,14 @@ func _input(event):
 	else:
 		isProcessed = false
 	labelText.set_text(str(text))
+	#print(text)
 
 func _on_btn_scann_pressed():
 	labelOutput.set_text("")
 	Globals.set("split", output.split("\n"))
 	output = ""
 	setScene("res://scenes/result.tscn")
-	print(Globals.get("split"))
+	#print(Globals.get("split"))
 
 # create a function to switch between scenes 
 func setScene(scene):
--- scripts/result.gd
+++ scripts/result.gd
@@ -7,7 +7,17 @@ func _ready():
 	var containerRow = get_node("cnt_table/cnt_statesInstance")
 	var duplicate = containerRow.duplicate()
 	duplicate.show()
-	print(duplicate.get_global_pos())
-	var labelInput = get_node("lbl_AFD")
+	#print(duplicate.get_global_pos())
+	var labelAlpha = get_node("lbl_alphabeth")
 	var split = Globals.get("split")
-	labelInput.set_text(split[0])
+	labelAlpha.set_text(str(alphabethSymbol(split)))
+
+func alphabethSymbol(alphaText):
+	var symbol = ""
+	var scann = RegEx.new()
+	scann.compile("; \\d")
+	scann.find(alphaText[0])
+	var symbols = scann.get_captures()
+	symbols = str(symbols)
+	symbol = symbols.substr(3,1)
+	return symbol
