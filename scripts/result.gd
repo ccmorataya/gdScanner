d73c2e72900ae3130945efb5d3094cdcc60f885f
Cristian Morataya <cris.morataya@gmail.com>
2016-11-16 10:37:56 -0600

    Improve the construction of the alphabeth field

--- scripts/result.gd
+++ scripts/result.gd
@@ -10,13 +10,22 @@ func _ready():
 	#print(duplicate.get_global_pos())
 	var labelAlpha = get_node("lbl_alphabeth")
 	var split = Globals.get("split")
-	labelAlpha.set_text(str(alphabethSymbol(split)))
+	split.remove(split.size()-1)
+	var lastItem = split[split.size()-1]
+	var alphabeth = "{"
+	for x in split:
+		alphabeth += alphabethSymbol(x)
+		var compString = x.match(lastItem)
+		if (!compString):
+			alphabeth += ", "
+	alphabeth += "}"
+	labelAlpha.set_text(alphabeth)
 
 func alphabethSymbol(alphaText):
 	var symbol = ""
 	var scann = RegEx.new()
 	scann.compile("; \\d")
-	scann.find(alphaText[0])
+	scann.find(alphaText)
 	var symbols = scann.get_captures()
 	symbols = str(symbols)
 	symbol = symbols.substr(3,1)
