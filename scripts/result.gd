a27e629d640eb5b987f89e6d79a2dab6c66226bf
Cristian Morataya <cris.morataya@gmail.com>
2016-11-16 22:32:25 -0600

    Aproach to duplicate node on the go

--- scripts/result.gd
+++ scripts/result.gd
@@ -8,12 +8,14 @@ var repeatedSymbols
 func _ready():
 	var containerRow = get_node("cnt_table/cnt_statesInstance")
 	var duplicate = containerRow.duplicate()
+	self.add_child(duplicate)
+	duplicate.set_owner(self)
 	duplicate.show()
-	#print(duplicate.get_global_pos())
 	var labelAlpha = get_node("lbl_alphabeth")
 	var split = Globals.get("split")
 	split.remove(split.size()-1)
 	var alphabethArray = []
+	
 	for x in split:
 		alphabethArray.append(alphabethSymbol(x))
 	
@@ -44,12 +46,10 @@ func check_duplicates(a):
 	var returnedA = []
 	seen[a[0]] = true
 	for i in range(1, a.size()):
-		
 		var v = a[i]
 		if seen.has(v):
 			bool_trigger = false
 		else:
 			seen[v] = true
 		returnedA.append(a[i])
-		#print(a[i])
 	return returnedA
