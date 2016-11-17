4b6087756a2955afdcb562ab428dbe314259fc44
Cristian Morataya <cris.morataya@gmail.com>
2016-11-16 23:17:56 -0600

    Improve the elimination of duplicated states

--- scripts/result.gd
+++ scripts/result.gd
@@ -4,6 +4,7 @@ extends Node2D
 var vector = Vector2(100,100)
 var bool_trigger
 var repeatedSymbols
+var repeatedStates
 
 func _ready():
 	var symbolA = get_node("cnt_table/cnt_statesInstance/lbl_tableRowSymbol")
@@ -20,22 +21,28 @@ func _ready():
 	var split = Globals.get("split")
 	split.remove(split.size()-1)
 	var alphabethArray = []
+	var stateArray = []
 	
 	for x in split:
 		alphabethArray.append(alphabethSymbol(x))
+		stateArray.append(afdState(x))
 	
 	repeatedSymbols = check_duplicates(alphabethArray)
-	var lastItem = repeatedSymbols[repeatedSymbols.size()-1]#split[split.size()-1]
+	repeatedStates = check_duplicates(stateArray)
+	
+	var lastSymbol = repeatedSymbols[repeatedSymbols.size()-1]#split[split.size()-1]
+	var lastState = repeatedStates[repeatedStates.size()-1]
+	
 	var alphabeth = "{"
 	for y in repeatedSymbols:
 		alphabeth += y
-		var compString = y.match(lastItem)
+		var compString = y.match(lastSymbol)
 		if (!compString):
 			alphabeth += ", "
 	
 	alphabeth += "}"
 	
-	#repeatedSymbols used in the table labels
+	#repeatedSymbols showed in the table labels
 	symbolA.set_text(repeatedSymbols[0])
 	symbolB.set_text(repeatedSymbols[1])
 	
@@ -67,9 +74,9 @@ func check_duplicates(a):
 func afdState(alphaText):
 	var symbol = ""
 	var scann = RegEx.new()
-	scann.compile("; \\d")
+	scann.compile("\\w\\d;")
 	scann.find(alphaText)
 	var symbols = scann.get_captures()
 	symbols = str(symbols)
-	symbol = symbols.substr(3,1)
+	symbol = symbols.substr(1, 2)
 	return symbol
