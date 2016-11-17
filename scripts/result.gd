502a934ce5b7e48e378c0f444859d9d0096fad81
Cristian Morataya <cris.morataya@gmail.com>
2016-11-16 22:19:29 -0600

    Exclude the duplicates symbols in the alphabeth

--- scripts/result.gd
+++ scripts/result.gd
@@ -2,6 +2,8 @@
 extends Node2D
 
 var vector = Vector2(100,100)
+var bool_trigger
+var repeatedSymbols
 
 func _ready():
 	var containerRow = get_node("cnt_table/cnt_statesInstance")
@@ -11,13 +13,19 @@ func _ready():
 	var labelAlpha = get_node("lbl_alphabeth")
 	var split = Globals.get("split")
 	split.remove(split.size()-1)
-	var lastItem = split[split.size()-1]
-	var alphabeth = "{"
+	var alphabethArray = []
 	for x in split:
-		alphabeth += alphabethSymbol(x)
-		var compString = x.match(lastItem)
+		alphabethArray.append(alphabethSymbol(x))
+	
+	repeatedSymbols = check_duplicates(alphabethArray)
+	var lastItem = repeatedSymbols[repeatedSymbols.size()-1]#split[split.size()-1]
+	var alphabeth = "{"
+	for y in repeatedSymbols:
+		alphabeth += y
+		var compString = y.match(lastItem)
 		if (!compString):
 			alphabeth += ", "
+	
 	alphabeth += "}"
 	labelAlpha.set_text(alphabeth)
 
@@ -30,3 +38,18 @@ func alphabethSymbol(alphaText):
 	symbols = str(symbols)
 	symbol = symbols.substr(3,1)
 	return symbol
+
+func check_duplicates(a):
+	var seen = {}
+	var returnedA = []
+	seen[a[0]] = true
+	for i in range(1, a.size()):
+		
+		var v = a[i]
+		if seen.has(v):
+			bool_trigger = false
+		else:
+			seen[v] = true
+		returnedA.append(a[i])
+		#print(a[i])
+	return returnedA
